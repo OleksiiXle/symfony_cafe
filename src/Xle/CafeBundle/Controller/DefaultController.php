30c84794491257f5acc9271bd8affe59c43d5bde
oleksiXle <lokoko.xle@ukr.net>
2018-08-07 10:24:22 +0300

    add map jquery

--- src/Xle/CafeBundle/Controller/DefaultController.php
+++ src/Xle/CafeBundle/Controller/DefaultController.php
@@ -8,6 +8,13 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('XleCafeBundle:Default:index.html.twig');
+      //  return $this->render('XleCafeBundle:Default:index.html.twig');
+        return $this->render('XleCafeBundle:Default:gmap.html.twig');
+
+    }
+
+    public function mapAction()
+    {
+        return $this->render('XleCafeBundle:Default:gmap.html.twig');
     }
 }
